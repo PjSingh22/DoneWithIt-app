bb7e9829a328190a8c8ed8e2dac8ee5199fbbdee
Prabhjot Singh <pjsingh1359@gmail.com>
2022-06-01 11:56:13 -0700

    add landing and image page

--- App.js
+++ App.js
@@ -1,31 +1,9 @@
 import { View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={{
-      backgroundColor: '#fff',
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      alignContent: 'center',
-      flexWrap: 'wrap',
-    }}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        width: 100,
-        height: 100,
-      }} />
-      <View style={{
-        backgroundColor: 'gold',
-        width: 100,
-        height: 100,
-      }} />
-      <View style={{
-        backgroundColor: 'tomato',
-        width: 100,
-        height: 100,
-      }} />
-    </View>
+    <ViewImageScreen />
   );
 }
